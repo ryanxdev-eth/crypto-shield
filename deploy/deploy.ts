1f3071b9f32c4f10db70da542d961728afe6b47e
ryanxdev-eth <jdfere69458@outlook.com>
2025-12-23 16:38:26 +0800

    Wire deploy script for CZama and CryptoShield

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,25 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedCZama = await deploy("CZama", {
     from: deployer,
+    args: [deployer],
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const deployedCryptoShield = await deploy("CryptoShield", {
+    from: deployer,
+    args: [deployedCZama.address],
+    log: true,
+  });
+
+  const czama = await hre.ethers.getContractAt("CZama", deployedCZama.address);
+  const tx = await czama.updateMinter(deployedCryptoShield.address);
+  await tx.wait();
+
+  console.log(`CZama contract: `, deployedCZama.address);
+  console.log(`CryptoShield contract: `, deployedCryptoShield.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_cryptoShield";
+func.tags = ["CZama", "CryptoShield"];
